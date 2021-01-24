29dcf1afc2038386991e3cd3476c0201f1d709ec
ajarochka <ajarochka@gmail.com>
2021-01-25 00:23:25 +0600

    Added tags, author, vacancies, resumes to models.py

--- main/models.py
+++ main/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
+TAGS = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+    ('Internship', 'Internship'),
+    ('Remote', 'Remote'),
+    ('Online', 'Online'),
+    ('35-44', '35-44'),
+    ('45-54', '45-54'),
+    ('55-64', '55-64'),
+    ('65+', '65+'),
+)
 class Author(models.Model):
-    name = models.CharField(max_length=128)
+    user = models.CharField(max_length=128)
 
 class Vacancies(Author):
     pass
